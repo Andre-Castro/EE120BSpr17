5843d7ea8e417091d3df20fd4a7ed29ef0ae0d6c
Andre-Castro <acast050@ucr.edu>
2017-05-31 00:14:54 -0700

    added some ADC stuff to code

--- acast050_FinalProject/main.c
+++ acast050_FinalProject/main.c
@@ -7,13 +7,29 @@
 
 #include <avr/io.h>
 
+void ADC_init() {
+	ADCSRA |= (1 << ADEN) | (1 << ADSC) | (1 << ADATE);
+	// ADEN: setting this bit enables analog-to-digital conversion.
+	// ADSC: setting this bit starts the first conversion.
+	// ADATE: setting this bit enables auto-triggering. Since we are
+	//        in Free Running Mode, a new conversion will trigger whenever
+	//        the previous conversion completes.
+}
 
 int main(void)
 {
+	unsigned short x = ADC;
 	DDRA = 0x00; PORTA = 0xFF;
 	DDRB = 0xFF; PORTB = 0x00;
+	ADC_init();
+	unsigned short temp = x;
+	unsigned short orig = x;
     while (1){
-		PORTB = (PINA & 0x10);
+		x = ADC;
+		if(x != temp || x != orig){
+			temp = x;
+		}
+		PORTB = temp & 0xFF;
     }
 }
 
