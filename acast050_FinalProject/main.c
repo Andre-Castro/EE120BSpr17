a7d15ed56af62b7ed6ef130a374aa40fb5918db4
Andre-Castro <acast050@ucr.edu>
2017-06-04 14:53:09 -0700

    Added IR pulsing code

--- acast050_FinalProject/main.c
+++ acast050_FinalProject/main.c
@@ -6,10 +6,12 @@
  */ 
 #include <avr/io.h>
 #include "io.c"
+#include "bit.h"
+
+unsigned char pulses[100][2];
 
 const unsigned char ARR_SIZE = 5;
 const char *strings[5] = {"Channel 1", "Channel 2", "Channel 3", "Channel 4", "Channel 5"};
-
 
 volatile unsigned char TimerFlag = 0;
 unsigned long _avr_timer_M = 1;
@@ -55,13 +57,13 @@ void ADC_init() {
 	//        the previous conversion completes.
 }
 
-enum JS_STATES {init, up, down} JS_STATE;
+enum JS_STATES {init, up, down, maintainD, maintainU} JS_STATE;
 void joyStick_Tick(){
 	static unsigned char pos = 0;
-	unsigned char J = ~(PINC & 0x01);
+	unsigned char J = ~PINA & 0x08;
 	switch(JS_STATE){ //transitions
 		case init:
-			if(J == 1 && pos < ARR_SIZE){
+			if(J > 0 && pos < ARR_SIZE - 1){
 				JS_STATE = down;
 			}
 			else{
@@ -71,6 +73,17 @@ void joyStick_Tick(){
 		case up:
 			break;
 		case down:
+			JS_STATE = maintainD;
+			break;
+		case maintainD:
+			if(J == 0){
+				JS_STATE = init;
+			}
+			else{
+				JS_STATE = maintainD;
+			}
+			break;
+		case maintainU:
 			break;
 		default:
 			JS_STATE = init;
@@ -85,16 +98,35 @@ void joyStick_Tick(){
 			pos++;
 			LCD_DisplayString(1, strings[pos]);
 			break;
+		case maintainD:
+			break;
+		case maintainU:
+			break;
 		default:
 			LCD_DisplayString(1, strings[0]);
 			break;
 	}
 }
 
+void irLoop(){
+	unsigned char highPulse = 0;
+	unsigned char lowPulse = 0;
+	static unsigned char currentPulse = 0;
+
+	while(PINA & 0x01){
+		highPulse++;
+	}
+	pulses[currentPulse][0] = highPulse;
+	while(!(PINA & 0x01)){
+		lowPulse++;
+	}
+	pulses[currentPulse][1] = lowPulse;
+}
+
 int main(void){
 	DDRA = 0x00; PORTA = 0xFF; //Input
-	DDRB = 0xFF; PORTB = 0x00; // PORTB set to output, outputs init 0s
-	DDRC = 0x00; PORTC = 0xFF; // PC7..4 outputs init 0s, PC3..0 inputs init 1s
+	DDRB = 0xFF; PORTB = 0x00; //Ouptut
+	DDRC = 0x0F; PORTC = 0xF0; 
 	DDRD = 0xFF; PORTD = 0x00;
 	ADC_init();
 	LCD_init();
@@ -108,11 +140,25 @@ int main(void){
 	LCD_DisplayString(1, strings[0]);
 	
 	unsigned short x = ADC;
+	unsigned char count2 = 0;
+	unsigned char nineAndTen = 0;
 
 	while (1){
 		joyStick_Tick();
-		x = ADC;
-		PORTB = x & 0xFF;
+		while(count2 < 50){
+			while (!TimerFlag);
+			TimerFlag = 0;
+			count2 ++;
+
+			irLoop();
+
+			x = ADC;
+			PORTB = x & 0xFF;
+			nineAndTen = (char)(x>>8);
+			PORTC = SetBit(PORTC, 2, (nineAndTen & 0x01));
+			PORTC = SetBit(PORTC, 3, (nineAndTen & 0x02));
+		}
+		count2 = 0;
 	}
 	return 0;
 }
